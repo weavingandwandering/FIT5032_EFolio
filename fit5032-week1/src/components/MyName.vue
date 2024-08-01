98e1828810f827b40c829c841b9c961b66424282
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2024-08-01 13:51:27 +0800

    adding week 1 and week 2 content in repo

---
+++ fit5032-week1/src/components/MyName.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1>
+      Ishita Gupta
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+h1 {
+  font-size: 1.2rem;
+  color: aqua;
+}
+</style>
