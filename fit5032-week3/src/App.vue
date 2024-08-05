f5706dda4ba3998987dbb75c7e33e4e973f46790
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2024-08-05 10:18:37 +0800

    Task 2

--- fit5032-week3/src/App.vue
+++ fit5032-week3/src/App.vue
@@ -1,14 +1,16 @@
 <script setup>
-import JSON from './components/JSON.vue';
+// import JSON from './components/JSON.vue';
+import Form from './components/Form.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
 
-    <div class="wrapper">
+    <!-- <div class="wrapper">
       <JSON msg="You did it!" />
-    </div>
+    </div> -->
+    <Form />
   </header>
 
   <main>
