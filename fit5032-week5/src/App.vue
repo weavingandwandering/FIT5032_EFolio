34401b6e1618d6c885fd38fabc43c5d11d09c2d4
Ishita <80767328+weavingandwandering@users.noreply.github.com>
2024-08-19 09:05:29 +0800

    Lab Setup

--- fit5032-week5/src/App.vue
+++ fit5032-week5/src/App.vue
@@ -1,17 +1,18 @@
 <script setup>
 // import JSON from './components/JSON.vue';
 import Form from './components/Form.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> 
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> 
 
      <div class="wrapper">
       <JSON msg="You did it!" />
-    </div>
-    
-  </header> -->
+    </div> -->
+    <Header/>
+  </header>
 
   <main>
     <Form />
